5294ea98d323237350f8d08fee5f27623a6e46ab
Polyane Tuag <pollyportugues@hotmail.com>
2020-10-15 18:30:09 -0300

    Lógica de rota para criar um orfanato

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphanages_view';
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async index(request: Request,response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
+
+    return response.json(orphanageView.renderMany(orphanages))
+  },
+
+  async show(request: Request,response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
+
+    return response.json(orphanageView.render(orphanage))
+  },
+
+  async create(request: Request,response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body
+  
+    const orphanagesRepository = getRepository(Orphanage);
+    
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return {path: image.filename}
+    }) 
+  
+    const orphanage = orphanagesRepository.create({   // cria um orfanato
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    });
+    console.log('entrou aqui', orphanage)
+    await orphanagesRepository.save(orphanage);             // salvar o orfanato
+  
+  
+    return response.status(201).json(orphanage);
+  }
+};
