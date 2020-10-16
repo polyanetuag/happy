625fba8b05f1d249ba0ee8f72c692aa9345dfe1b
Polyane Tuag <pollyportugues@hotmail.com>
2020-10-16 19:28:56 -0300

    Listando orfanatos no mapa

--- web/src/ screens/OrphanagesMap.tsx
+++ web/src/ screens/OrphanagesMap.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import {FiPlus, FiArrowRight} from 'react-icons/fi';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg'
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import '../styles/pages/orphanages-map.css';
-import mapIcon from '../utils/mapIcon';
 
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
-function OrphanagesMap() {
+function OrphanagesMap() {      // chamada de uma api
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -36,17 +51,22 @@ function OrphanagesMap() {
         url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
         />
 
-        <Marker 
+        {orphanages.map(orphanage => {
+          return (
+            <Marker 
           icon={mapIcon}
-          position={[-20.3505594,-40.2986506]}
+          position={[orphanage.latitude, orphanage.longitude]}
+          key={orphanage.id}
         >
           <Popup closeButton={false} minWidth={240} maxWidth={240} className= "map-popup">
-            Lar das Meninas
-            <Link to="/orphanages/1">
+            {orphanage.name}
+            <Link to={`/orphanages/${orphanage.id}`}>
               <FiArrowRight size={20} color="#FFF" />
             </Link>
           </Popup>
           </Marker>
+          )
+        })}
       </Map>
 
       <Link to="/orphanages/create" className="create-orphanage"> 
